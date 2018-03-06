d2381f36db7f1171dd635677a22ebc915dc27fc9
eichwana <tirisfal.guardian@gmail.com>
2018-03-06 18:51:12 +0000

    Save list to file
    Creates C:\codetest and saves a text file here with the text entered

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ namespace ConsoleApp1
     {
         public static void Main()
         {
+            string nametext;
+            string nameshow = "Write your name:";
             string h1 = "Type 3 entries, press enter after each entry";
             string e1;
             string e2;
             string e3;
 
+            Console.WriteLine(nameshow);
+            nametext = Console.ReadLine();
             Console.WriteLine(h1);
             e1 = Console.ReadLine();
             e2 = Console.ReadLine();
@@ -18,9 +22,10 @@ namespace ConsoleApp1
             Console.WriteLine("\n1. " + e1);
             Console.WriteLine("2. " + e2);
             Console.WriteLine("3. " + e3);
-
-
-
+            string filepath = "C:\\codetest\\";
+            string[] entries = { e1, e2, e3 };
+            System.IO.File.WriteAllLines(filepath + nametext + ".txt", entries);
+            Console.WriteLine("See "+filepath + nametext + ".txt");
 
             Console.ReadLine(); // Keeps the console open by waiting for enter
 
