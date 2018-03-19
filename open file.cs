927202cf698811231a3414ce8c41b54aa4816ee1
eichwana <tirisfal.guardian@gmail.com>
2018-03-19 23:18:27 +0000

    v2.1
    Allows user to specify number of entries.
    Numbers entries before they're saved in the file.
    When loading entries, puts them into an array of strings

--- open file.cs
+++ open file.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace open_file
 {
@@ -7,33 +8,55 @@ namespace open_file
         static void Main(string[] args)
         {
             string filepath = "C:\\codetest\\";
-
+            string errortext = "Sorry, I don't understand...";
+            
             Console.WriteLine("Do you have an existing file?");
             string filee = Console.ReadLine();
-            if (filee.Trim() == "n" | filee.Trim() == "no" | filee.Trim() == "0" | filee.Trim() == "N" | filee.Trim() == "No")
+
+            if (filee.Trim() == "n" | filee.Trim() == "no" | filee.Trim() == "0" | filee.Trim() == "N" | filee.Trim() == "No" | filee.Trim() == "niet")
             {
-                Console.WriteLine("What is your name?");
+                List<string> entries2 = new List<string>();
+
+                Console.WriteLine("\nWhat is your name?");
                 String name = Console.ReadLine();
-                Console.WriteLine("Hello " + name + ", type your entries and press enter after each one");
-                string e1 = Console.ReadLine();
-                string e2 = Console.ReadLine();
-                string e3 = Console.ReadLine();
-                
+                Console.WriteLine("Hello " + name + ", how many entries do you have?");
+                string num = Console.ReadLine();
+                int num2 = Convert.ToInt16(num);
+                Console.WriteLine("Please type your " + num + " entries, pressing enter after each one");
+
+                for (int n = 1; n <= num2; n++)
+                    {
+                        string t = Console.ReadLine();
+                        string e = n + ". " + t;
+                        entries2.Add(e);
+                    }
+
                 System.IO.Directory.CreateDirectory(filepath);
-                string[] entries = { e1, e2, e3 };
-                System.IO.File.WriteAllLines(filepath + name + ".txt", entries);
+                System.IO.File.WriteAllLines(filepath + name + ".txt", entries2);
                 Console.WriteLine("See " + filepath + name + ".txt");
             }
+
             else if (filee.Trim() == "y" | filee.Trim() == "Y" | filee.Trim() == "yes" | filee.Trim() == "1" | filee.Trim() == "Yes")
             {
-                Console.WriteLine("What is your filename?");
+                Console.WriteLine("\nWhat is your filename?");
                 string efile = Console.ReadLine();
-                string lines = System.IO.File.ReadAllText(filepath + efile + ".txt");
-                Console.WriteLine(lines);
+                string[] entries = System.IO.File.ReadAllLines(filepath + efile + ".txt");
+
+                // PLACEHOLDER TEXT
+                // if efile doens't exist
+                // display errortext
+
+
+                Console.WriteLine("\nContents of " + efile + "\n");
+                foreach (string entry in entries)
+                {                
+                    Console.WriteLine(entry);
+                }
             }
+
             else
             {
-                Console.WriteLine("Sorry, I don't understand...");
+                Console.WriteLine(errortext);
             }
 
             Console.ReadLine(); // Keeps the console open by waiting for enter
